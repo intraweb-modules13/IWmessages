d8193a7d956d365eb98110ae2652724b7f52a37d
aperezm <aperezm@e968f4be-1634-4921-b96b-e3dd0a78059a>
2011-02-11 09:46:17 +0000

    Refactoring modules to 1.3

---
+++ lib/IWmessages/Version.php
@@ -0,0 +1,26 @@
+<?php
+ /**
+ * Load the module version information
+ *
+ * @author		Albert Pérez Monfort (intraweb@xtec.cat)
+ * @return		The version information
+ */
+$dom = ZLanguage::getModuleDomain('IWmessages');
+$modversion['name'] = 'IWmessages';
+$modversion['version'] = '2.0';
+$modversion['description'] = __('Allow to send private messages between users', $dom);
+$modversion['displayname']    = __('IWMessages', $dom);
+$modversion['url']    = __('IWmessages', $dom);
+$modversion['credits'] = 'pndocs/credits.txt';
+$modversion['help'] = 'pndocs/help.txt';
+$modversion['changelog'] = 'pndocs/changelog.txt';
+$modversion['license'] = 'pndocs/license.txt';
+$modversion['official'] = 0;
+$modversion['author'] = 'Richard Tirtadji & Albert Pérez Monfort';
+$modversion['contact'] = 'rtirtadji@hotmail.com & aperezm@xtec.es';
+$modversion['admin'] = 1;
+$modversion['securityschema'] = array('IWmessages::' => '::');
+$modversion['dependencies'] = array(array('modname' => 'IWmain',
+											'minversion' => '2.0',
+											'maxversion' => '',
+											'status' => ModUtil::DEPENDENCY_REQUIRED));
